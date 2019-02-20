b169047bfab4a13e5a93a6823bea37548fd02110
am1999 <ankita.maurya_ccv17@gla.ac.in>
2019-02-20 11:08:48 +0530

    add getter and setter

--- src/Main/linkedList1.java
+++ src/Main/linkedList1.java
@@ -1,5 +1,13 @@
 package Main;
-
+//Node
 public class linkedList1 {
     private int data;
+
+    public int getData() {                //Alt+insert
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
 }

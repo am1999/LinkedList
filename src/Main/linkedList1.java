79fde32794db92129ce8e55b04a17dfa31e89c4a
am1999 <ankita.maurya_ccv17@gla.ac.in>
2019-02-20 11:17:57 +0530

    added constructors

--- src/Main/linkedList1.java
+++ src/Main/linkedList1.java
@@ -2,12 +2,29 @@ package Main;
 //Node
 public class linkedList1 {
     private int data;
-
+private linkedList1 next;
     public int getData() {                //Alt+insert
         return data;
     }
 
     public void setData(int data) {
         this.data = data;
+
+    }
+
+    public linkedList1 getNext() {
+        return next;
+    }
+
+    public void setNext(linkedList1 next) {
+        this.next = next;
+    }
+
+    public linkedList1(int data) {
+        this.data = data;
+    }
+
+    public linkedList1(linkedList1 next) {
+        this.next = next;
     }
 }
